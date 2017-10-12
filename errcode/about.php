8566e945bf4999b69e6f3eae5546c11c8c296430
bestbai <kunlun.bai@gmail.com>
2017-10-12 22:53:14 +0800

    Create about.php

---
+++ errcode/about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 
+<title>最新消息</title>
+<style>
+html,body{ overflow:hidden;}
+*{margin:3; padding:3;}
+body{background-color:#000000; width:100%; }
+a{color:#2F63B3;text-decoration:none;outline-style:none;}a:hover{color:#0040FF;text-decoration:underline;}
+.news{ margin:0 auto; color:#EEE; font-size:12px;}
+</style>
+</head>
+
+<body><div id="" class="news">
+	<p>本页二维码如下，如果你在微信中打开可能无法下载，可从右上角的选项中点击“从浏览器中打开”。</p>
+</div>
+
+<div><img src="http://bestbai.qiniudn.com/about.png" width="" height="" border="0" alt="二维码">
+</div>
+<p>暂无可用服务器，请忍耐！</p>
+
+</body>
+</html>
